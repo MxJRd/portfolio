c2b136a802bc255db71730a276c685485d6ecb09
MxJRd <reed182@mail.chapman.edu>
2020-04-26 21:14:03 -0700

    completed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,52 @@
-console.log("Index.js Loaded");
+import $ from 'jquery';
+/***************************************************************************** */
+function beginScroll() {
+    $('.nav-item').on('click', function () {
+        $('html, body').animate({
+            scrollTop: $(".about-title-container").offset().top
+        }, 825);
+    });
+}
+/***************************************************************************** */
+// function changelinkedinImgHtml(){
+//     return $(`.linkedin`).html(`<p >Click worked</p>`)
+// }
 
-function(){
-    $(`.linkedin`).on('click', function(){
-        $(`.linkedin`).append(`<p>Click worked</p>`)
+function linkedinImgClick() {
+    $(`.linkedinImg`).on('click', function () {
+        // changelinkedinImgHtml();
+        window.open("https://www.linkedin.com/");
     })
 }
+/***************************************************************************** */
+// function changeGithubImgHtml(){
+//     return $(`.github`).html(`<p >Click worked</p>`)
+// }
+
+function githubImgClick() {
+    $(`.githubImg`).on('click', function () {
+        // changeGithubImgHtml();
+        window.open("https://www.github.com/")
+    })
+}
+/***************************************************************************** */
+// function changeImgHtml(){
+//     return $(`.email`).html(`<p >Click worked</p>`)
+// }
+
+function emailImgClick() {
+    $(`.emailImg`).on('click', function () {
+        // changeImgHtml();
+        window.location.href = "mailto:user@example.com?subject=Subject&body=message%20goes%20here";
+    })
+}
+/***************************************************************************** */
+$(document).ready(function () {
+    console.log("Index.js Loaded");
+    beginScroll();
+    linkedinImgClick();
+    githubImgClick();
+    emailImgClick();
+});
+
+
